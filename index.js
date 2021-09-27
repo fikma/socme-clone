be2faa2cfc3b320db4a42e401d63bc0029cc42ed
fikma <ade.fm97@gmail.com>
2021-09-27 11:52:31 +0700

    adding and setting up environment using package config
    add folder with name config while adding file development.json inside it.
    the file containing const name for database config, and app config which
    is used by the app.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const config = require('config');
+
 const app = express();
-const port = 1234;
 
 app.get('/', (req, res) => {
 	res.send({message: "Hello world"});
 });
 
-app.listen(port, () => {
-	console.log(`app running at port: ${port}`);
+app.listen(config.get("app.port"), () => {
+	console.log(`app running at port: ${config.get("app.port")}`);
 });
