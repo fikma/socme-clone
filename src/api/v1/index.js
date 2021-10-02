a98c060f837f005b525837553e81b772245304e0
fikma <ade.fm97@gmail.com>
2021-10-02 19:52:53 +0700

    add rest test using vsCode extension. url.http used for est test

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -4,4 +4,10 @@ router.get('/', (req, res) => {
 	res.send({message: "hello from api v1"});
 });
 
+router.post(`/data`, (req, res) => {
+	const data = req.body;
+
+	res.send(data);
+});
+
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 
+
 app.use('/api', require('./api/index'))
 
 app.listen(config.get("app.port"), () => {
